7b3b3dc0a224012a90f5c2b79881ca216c9f6fb7
Dip J <dj.nlx@netlox.io>
2022-09-07 12:14:35 +0900

    some more netutils added

--- netutils.go
+++ netutils.go
@@ -7,6 +7,9 @@ import (
 	"encoding/binary"
 	"net"
 	"unsafe"
+	"syscall"
+	"errors"
+	"bytes"
 )
 
 // Ntohl - Network to host byte-order long
@@ -62,4 +65,65 @@ func NltoIP(addr uint32) net.IP {
 	dip = append(dip, uint8(addr>>24&0xff))
 
 	return dip
+}
+
+// arpPing - sends a arp request given the DIP, SIP and interface name
+func arpPing(DIP net.IP, SIP net.IP, ifName string) (int, error) {
+	bZeroAddr := []byte{ 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 }
+	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_DGRAM, int(Htons(syscall.ETH_P_ARP)))
+	if err != nil {
+		return -1, errors.New("af-packet-err")
+	}
+	defer syscall.Close(fd)
+
+	if err := syscall.BindToDevice(fd, ifName); err != nil {
+		return -1, errors.New("bind-err")
+	}
+
+	ifi, err := net.InterfaceByName(ifName)
+	if err != nil {
+		return -1, errors.New("intf-err")
+	}
+
+	ll := syscall.SockaddrLinklayer{
+		Protocol: Htons(syscall.ETH_P_ARP),
+		Ifindex:  ifi.Index,
+		Pkttype:  0, // syscall.PACKET_HOST
+		Hatype:   1,
+		Halen:    6,
+	}
+
+	for i := 0; i < 6; i++ {
+		ll.Addr[i] = 0xff
+	}
+
+	buf := new(bytes.Buffer)
+
+	var sb = make([]byte, 2)
+	binary.BigEndian.PutUint16(sb, 1)  // HwType = 1
+	buf.Write(sb)
+
+	binary.BigEndian.PutUint16(sb, 0x0800)  // protoType
+	buf.Write(sb)
+
+	buf.Write([]byte{6}) // hwAddrLen
+	buf.Write([]byte{4}) // protoAddrLen
+
+	binary.BigEndian.PutUint16(sb, 0x1) // OpCode
+	buf.Write(sb)
+
+	buf.Write(ifi.HardwareAddr) // senderHwAddr
+	buf.Write(SIP.To4()) // senderProtoAddr
+
+	buf.Write(bZeroAddr) // targetHwAddr
+	buf.Write(DIP.To4()) // targetProtoAddr
+
+	if err := syscall.Bind(fd, &ll); err != nil {
+		return -1, errors.New("bind-err")
+	}
+	if err := syscall.Sendto(fd, buf.Bytes(), 0, &ll); err != nil {
+		return -1, errors.New("send-err")
+	}
+
+	return 0, nil
 }
